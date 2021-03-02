8faa788b0968fda09721ceb3cb1896fcdc855dc1
thiagosantana18 <thigoncalves64@gmail.com>
2021-03-02 20:18:31 -0300

    Primeiro Projeto SpringBoot

---
+++ src/main/java/br/com/senac/ProjetoSenacApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSenacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSenacApplication.class, args);
+	}
+
+}
